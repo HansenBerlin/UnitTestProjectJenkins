6a51706b1595fd42444935778a77fc8863c69ea4
Hannes R <hansdrum@hotmail.com>
2020-09-22 12:18:37 +0200

    push

--- BreakMe/Program.cs
+++ BreakMe/Program.cs
@@ -15,7 +15,8 @@ namespace BreakMe
             View.ShowOutput();
 
             Console.WriteLine("Test push to master branch");
-           
+            Console.WriteLine("Test push to master branch");
+
 
 
 
